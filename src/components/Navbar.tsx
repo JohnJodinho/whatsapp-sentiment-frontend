ac4d708c0236898fb4d08c3f9b1c72e13ca44162
JohnJodinho <ibrahimjohn2708199719@gmail.com>
2025-10-20 11:02:42 +0100

    optimized for production

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState, useEffect } from "react"
+import { useState, useEffect } from "react"
 
 import { Menu, Home, Upload, LayoutDashboard, Info } from "lucide-react"
 import CustomButton from "@/components/custom-ui/projectbutton"
